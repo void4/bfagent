b05aef76d5f4023304cdf6c627d56a5beb188b09
void4 <void4@users.noreply.github.com>
2018-05-08 04:05:03 +0200

    4am - add 'smarter' bf code gen

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
+# BFAgent
 Simulates a world of agents running brainfuck
 
 - they have to eat
 
 - they can reproduce
 - they can communicate
-- they can clone themselves
 
 ![animated gif](https://raw.githubusercontent.com/void4/bfagent/master/anim.gif)
 
--- main.py
+++ main.py
@@ -5,17 +5,20 @@ from scipy.spatial import KDTree
 from PIL import Image, ImageDraw
 import numpy as np
 import imageio
+import bisect
+
+
 
 IMG_SCALE = 10
 MAX_IO_LEN = 10
 TAPE_SIZE = 128
-NUM_AGENTS = 20
-WORLD_SIZE = 40
+NUM_AGENTS = 30
+WORLD_SIZE = 50
 WORLD_DIM = 2
 
 class Agent:
 	def __init__(self, rot=None, pos=None, code=None):
-		self.energy = 100
+		self.energy = 600
 		if rot is None:
 			rot = rs(2*pi)
 		self.rot = rot
@@ -31,6 +34,7 @@ class Agent:
 		self.inp = []
 		self.out = []
 		self.near = 0
+		self.age = 0
 		self.reproduce = False
 		self.buildmap()
 	
@@ -59,13 +63,24 @@ class Agent:
 		self.inp = self.inp[:MAX_IO_LEN]
 	
 	def run(self, steps):
+		codelen = len(self.code)
+		tapelen = len(self.tape)
+
+		self.age += 1
+
+		self.energy -= 10
+		self.action = None
+		
+		self.attack = False
+		self.reproduce = False
+
 		for i in range(steps):
 			#print(self.code, self.ip, self.map)
-			codelen = len(self.code)
+
 			ipo = self.ip%codelen
 			com = self.code[ipo]
-			tapelen = len(self.tape)
-			self.reproduce = False
+
+
 			if com == ">":
 				self.pt += 1
 			elif com == "<":
@@ -81,22 +96,22 @@ class Agent:
 				if self.tape[self.pt%tapelen] != 0:
 					self.ip = self.map[ipo]	
 			elif com == ".":
-				self.out.append(self.tape[self.pt%tapelen])
+				self.action = [".", self.tape[self.pt%tapelen]]
 			elif com == ",":
 				if len(self.inp) > 0:
 					self.tape[self.pt%tapelen] = self.inp.pop(0)
 			elif com == "l":
-				self.rot -= 0.01
+				self.action = "l"
 			elif com == "r":
-				self.rot += 0.01
+				self.action = "r"
 			elif com == "f":
-				self.pos[0] += sin(self.rot) * 0.1
-				self.pos[1] += cos(self.rot) * 0.1
+				self.action = "f"
 			elif com == "x":
-				if self.energy > 300:
-					self.reproduce = True
+				self.action = "x"
 			elif com == "n":
 				self.tape[self.pt%tapelen] = self.near
+			elif com == "a":
+				self.action = "a"
 			"""
 			elif com == "c":
 				if self.energy > 200:
@@ -107,7 +122,27 @@ class Agent:
 					agents.append(child)
 			"""
 			self.ip += 1
-
+		
+		if self.action is None:
+			pass
+		elif self.action == "f":
+			self.pos[0] += sin(self.rot) * 0.1
+			self.pos[1] += cos(self.rot) * 0.1
+			self.energy -= 3
+		elif self.action == "l":
+			self.rot -= 0.01
+			self.energy -= 2
+		elif self.action == "r":
+			self.rot += 0.01
+			self.energy -= 2
+		elif self.action == "x":
+			if self.energy > 800:		
+				self.reproduce = True
+		elif self.action == "a":
+			self.attack = True
+			#self.energy -= 4
+		elif self.action[0] == ".":
+			self.out.append(self.action[1])
 # Random scaled number
 def rs(scale):
 	return random()*scale
@@ -136,10 +171,74 @@ def av(v1, v2):
 def rect(pos, scale):
 	return [pos[0]-scale,pos[1]-scale,pos[0]+scale,pos[1]+scale]
 
+# Implements weighted choice selection
+def cdf(weights):
+    total = sum(weights)
+    result = []
+    cumsum = 0
+    for w in weights:
+        cumsum += w
+        result.append(cumsum / total)
+    return result
+
+def weighted_choice(population, weights):
+    assert len(population) == len(weights)
+    cdf_vals = cdf(weights)
+    x = random()
+    idx = bisect.bisect(cdf_vals, x)
+    return population[idx]
+
+instr = "><+-.,[]lrfxn"#c
+weights = [100,100,100,100,4,4,10,10,7,7,10,7,10]
+combined = list(zip(instr, weights))
+MAXCODELEN = 128
+def gen_weighted():
+	return "".join(weighted_choice(instr, weights) for i in range(randint(1,MAXCODELEN)))
+
+conflicts = ["+-", "-+", "<>", "><", "rl", "lr", "[]"]
+
+def gen_alt():
+	code = choice(instr)
+	for i in range(MAXCODELEN):
+		allowed = list(combined)
+		for pair in conflicts:
+			if code[-1] == pair[0]:
+				allowed.remove(combined[instr.index(pair[1])])
+		c,w = list(zip(*allowed))
+		#print(code[-1], c)
+		code += weighted_choice(c,w)
+	#print(code)
+	return code
+
+
+# Optimizes bfagent code
+def optimize(code):
+	old = code
+	for i in range(len(code)//2):
+		opt = ""
+		i = 0
+		while i < len(old)-1:
+			if code[i]+old[i+1] in ["+-", "-+", "><", "<>"]:
+				print(code[i]+old[i+1])
+				i += 2
+			else:
+				opt += old[i]
+				i += 1
+	
+		old = opt
+	print(len(code)-len(old))
+	return old
+
 # Generate random code sequence
-instr = "><+-.,[]lrfcxn"
+
 def codegen():
-	return "".join(choice(instr) for i in range(randint(1,100)))
+	"""
+	while True:
+		code = optimize(gen_weighted())
+		if code:
+			return code
+	"""
+	return gen_alt()
 
 # Converts PIL image to numpy array
 def PIL2array(img):
@@ -184,7 +283,7 @@ def mutate(s1, s2):
 images = []
 
 agents = [Agent() for i in range(NUM_AGENTS)]
-food = [rp() for i in range(1000)]
+food = [rp() for i in range(WORLD_SIZE**2*5)]
 
 
 w = h = WORLD_SIZE*IMG_SCALE
@@ -200,40 +299,62 @@ def step(count):
 
 	tree = KDTree(food)
 	agtree = KDTree([agent.pos for agent in agents])
-	
+	#print(i)
 	if i%10 == 0:
-		print(len(agents))
+		energies = [agent.energy for agent in agents]
+		ages = [agent.age for agent in agents]
+		print("NumAgt:%i\tAvgAge:\t%i\tAvgEng:%i\tStdDev%i" % (len(agents), np.mean(ages), np.mean(energies), np.std(energies)))
 		food.append(rp())
+	
+	if len(agents) < NUM_AGENTS:
+		agents.append(Agent())
 
 	remove = []
 	for agentindex, agent in enumerate(agents):
-		agent.run(24)
+		agent.run(1024)
 		agent.pos = mv(WORLD_SIZE, agent.pos)
-		near = tree.query_ball_point(agent.pos, 0.5)
+		near = tree.query_ball_point(agent.pos, 0.2)
+		#print(len(near))
 		for index in near:
 			try:
 				food.pop(index)
 			except IndexError:
 				pass
-			agent.energy += 20
+			agent.energy += 80
 		agent.energy -= 1
-		if agent.energy < 0:
+		if agent.energy < 0 or agent.age**1.2 * 10 > agent.energy:
 			remove.append(agentindex)
 			continue
 		rwh = 0.1
 		draw.rectangle(sv(IMG_SCALE, rect(agent.pos, rwh)), fill="black")
 
-		if agent.reproduce:
-			near_agents = agtree.query_ball_point(agent.pos, 5)
+		if agent.attack:
+			near_agents = agtree.query_ball_point(agent.pos, 4)
+			try:
+				near_agents.remove(agentindex)
+			except ValueError:
+				pass
 			if len(near_agents) > 0:
-				#print("reproduced!")
+				agent.energy -= 12
+				other_agent = agents[near_agents[0]]
+				other_agent.energy -= 8
+
+		if agent.reproduce:
+			near_agents = agtree.query_ball_point(agent.pos, 1.5)
+			try:
+				near_agents.remove(agentindex)
+			except ValueError:
+				pass
+			if len(near_agents) > 0:
+				print("reproduced!", len(near_agents))
+				agent.energy -= 700
 				other_agent = agents[near_agents[0]]
 				new_pos = av(agent.pos, other_agent.pos)
 				new_code = mutate(agent.code, other_agent.code)
 				new_agent = Agent(pos=new_pos, code=new_code)
 				agents.append(new_agent)
 
-		near_agents = agtree.query_ball_point(agent.pos, 10)
+		near_agents = agtree.query_ball_point(agent.pos, 3)
 		agent.near = len(near_agents)
 		for b in near_agents:
 			if b != agentindex:
@@ -243,6 +364,7 @@ def step(count):
 		agent.out = []
 	#im.save("anim/%i.jpg" % i)
 	agents = [agent for agentindex, agent in enumerate(agents) if agentindex not in remove]
+
 	if count % 5 == 0:
 		images.append(PIL2array(im))
 
